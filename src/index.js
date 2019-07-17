1c9024f19fa7e2fe0b4fb98aa6ced7bae801b354
adevianin <anton.devianin@masterofcode.com>
2019-07-17 16:54:09 +0300

    coment

--- src/index.js
+++ src/index.js
@@ -4,4 +4,6 @@ import pageData from './pageData';
 
 domainFacade.initPage(pageData);
 
+// останній параметр(domainFacade.getPage()) не використовується, хз як його передать у Vue правильно. заімпортив напряму в файлі src/view/components/app.vue:9
+// але так менш очевидно
 initViewLayer(document.getElementById('app'), domainFacade.getPage());
